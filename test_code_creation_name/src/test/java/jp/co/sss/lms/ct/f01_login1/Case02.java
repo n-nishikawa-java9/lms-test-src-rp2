b1ebc86df11e18dd43eab630114792b69fbab41e
n-nishikawa <n.nishikawa.its.202509@gmail.com>
2026-01-26 09:18:44 +0900

    fix:Case02.java 受講生 ログイン 認証失敗のテストコードを追加
    ログイン認証失敗時のエラーメッセージが正しく表示されていることを確認するためのテストコードを追加した。
    以下のメッセージが期待する結果である。
    エラーメッセージ：* ログインに失敗しました。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +40,45 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// ログイン画面へ遷移
+		goTo("http://localhost:8080/lms");
+
+		//画面遷移確認
+		String title = webDriver.getTitle();
+		assertEquals("ログイン | LMS", title);
+
+		// エビデンスを取得する
+		getEvidence(new Object() {
+		}, "01");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// ログインIDとパスワードを入力
+		WebElement userId = WebDriverUtils.webDriver.findElement(By.name("loginId"));
+		WebElement passWord = WebDriverUtils.webDriver.findElement(By.name("password"));
+		userId.sendKeys("WrongID01");
+		passWord.sendKeys("WrongPass01");
+
+		//エビデンスを取得する
+		getEvidence(new Object() {
+		}, "02");
+
+		//「ログイン」ボタンをクリックする
+		WebDriverUtils.webDriver.findElement(By.className("btn-primary")).click();
+
+		//エラーメッセージ表示の待機時間
+		WebDriverUtils.visibilityTimeout(By.className("error"), 5);
+
+		//ログイン失敗のエラーメッセージ表示確認
+		WebElement errormessages = WebDriverUtils.webDriver.findElement(By.className("error"));
+		assertEquals("* ログインに失敗しました。", errormessages.getText());
+
+		getEvidence(new Object() {
+		}, "03");
+
 	}
 
 }
