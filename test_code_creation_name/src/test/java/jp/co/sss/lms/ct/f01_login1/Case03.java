6cec996492ebb388058b1d547cff7f7232879aaa
n-nishikawa <n.nishikawa.its.202509@gmail.com>
2026-01-27 10:18:03 +0900

    fix:Case03.java ログイン 正常系のテストコードを追加
    テストケース03のログイン画面から受講生アカウントでログインし、コース詳細画面に正しく遷移することを確認するためのテストコードを追加した。
    期待値は以下の通りである。
    遷移後の画面：コース詳細画面
    「コース詳細」と表示されていること

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +40,46 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// ログイン画面へ遷移
+		goTo("http://localhost:8080/lms");
+
+		//画面遷移確認
+		String title = webDriver.getTitle();
+		assertEquals("ログイン | LMS", title);
+
+		// エビデンスを取得する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// ログインIDとパスワードを入力
+		WebElement userId = WebDriverUtils.webDriver.findElement(By.name("loginId"));
+		WebElement passWord = WebDriverUtils.webDriver.findElement(By.name("password"));
+		userId.sendKeys("StudentAA01");
+		passWord.sendKeys("PasswordAA01");
+
+		//エビデンスを取得する
+		getEvidence(new Object() {
+		}, "01");
+
+		//「ログイン」ボタンをクリックする
+		WebDriverUtils.webDriver.findElement(By.className("btn-primary")).click();
+
+		//コース詳細画面表示の待ち時間
+		WebDriverUtils.visibilityTimeout(By.className("active"), 5);
+
+		//画面遷移確認
+		WebElement errormessages = WebDriverUtils.webDriver.findElement(By.className("active"));
+		assertEquals("コース詳細", errormessages.getText());
+
+		//エビデンスを取得する
+		getEvidence(new Object() {
+		}, "02");
+
 	}
 
 }
