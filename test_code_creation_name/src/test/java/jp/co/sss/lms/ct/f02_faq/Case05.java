65c8f0355cf3f90bc044ac0386ce96f5be2100ea
n-nishikawa <n.nishikawa.its.202509@gmail.com>
2026-01-28 14:13:43 +0900

    fix:Case05.java よくある質問画面でのキーワード検索とキーワードクリアのテストコードを追加
    初回ログイン済みの受講生アカウントでログインし、よくある質問画面で正しくキーワード検索が行われ、キーワードをクリアボタンで消去できることを確認するためのテストコードを追加した。
    期待値は以下の通り
    キーワード：研修
    検索結果：2件
    ・「助成金書類の作成方法が分かりません」
    ・「研修の申し込みはどのようにすれば良いですか？」
    クリア押下後
    キーワード消去

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
@@ -3,6 +3,9 @@ package jp.co.sss.lms.ct.f02_faq;
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Duration;
+import java.util.List;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -11,7 +14,11 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト よくある質問機能
@@ -38,41 +45,157 @@ public class Case05 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// ログイン画面へ遷移
+		goTo("http://localhost:8080/lms");
+
+		//画面遷移確認
+		String title = webDriver.getTitle();
+		assertEquals("ログイン | LMS", title);
+
+		// エビデンスを取得する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// ログインIDとパスワードを入力
+		WebElement userId = WebDriverUtils.webDriver.findElement(By.name("loginId"));
+		WebElement passWord = WebDriverUtils.webDriver.findElement(By.name("password"));
+		userId.sendKeys("StudentAA01");
+		passWord.sendKeys("PasswordAA01");
+
+		//エビデンスを取得する
+		getEvidence(new Object() {
+		}, "01");
+
+		//「ログイン」ボタンをクリックする
+		WebDriverUtils.webDriver.findElement(By.className("btn-primary")).click();
+
+		//コース詳細画面表示の待ち時間
+		WebDriverUtils.visibilityTimeout(By.className("active"), 5);
+
+		//画面遷移確認
+		WebElement errormessages = WebDriverUtils.webDriver.findElement(By.className("active"));
+		assertEquals("コース詳細", errormessages.getText());
+
+		//エビデンスを取得する
+		getEvidence(new Object() {
+		}, "02");
 	}
-	
+
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//ヘッダーの「機能」を押下し、ドロップダウンを表示
+		WebDriverUtils.webDriver.findElement(By.className("dropdown")).click();
+
+		//ドロップダウン表示タイムアウト設定
+		visibilityTimeout(By.linkText("ヘルプ"), 5);
+
+		//ドロップダウンの表示を確認
+		WebElement helpLink = WebDriverUtils.webDriver.findElement(By.linkText("ヘルプ"));
+		assertEquals("ヘルプ", helpLink.getText());
+
+		//エビデンスを取得する
+		getEvidence(new Object() {
+		}, "01");
+
+		//「ヘルプ」リンクを押下
+		WebDriverUtils.webDriver.findElement(By.linkText("ヘルプ")).click();
+		pageLoadTimeout(5);
+
+		//ヘルプ画面への画面遷移確認
+		WebElement helpWebElement = WebDriverUtils.webDriver.findElement(By.tagName("h2"));
+		assertEquals("ヘルプ", helpWebElement.getText());
+
+		//エビデンスを取得する
+		getEvidence(new Object() {
+		}, "02");
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		//ヘルプ画面の「よくある質問」リンクをクリック
+		WebDriverUtils.webDriver.findElement(By.linkText("よくある質問")).click();
+		pageLoadTimeout(5);
+		String currHandle = WebDriverUtils.webDriver.getWindowHandle();
+		assertNotNull(currHandle);
+
+		//タブの切り替え
+		Object[] windowHandles = WebDriverUtils.webDriver.getWindowHandles().toArray();
+		WebDriverUtils.webDriver.switchTo().window((String) windowHandles[1]);
+
+		//よくある質問画面への画面遷移確認
+		WebElement faqElement = WebDriverUtils.webDriver.findElement(By.tagName("h2"));
+		assertEquals("よくある質問", faqElement.getText());
+
+		//エビデンスを取得する
+		getEvidence(new Object() {
+		});
 	}
+
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 キーワード検索で該当キーワードを含む検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+		//よくある質問画面のキーワード検索にキーワードを入力する
+		WebDriverUtils.webDriver.findElement(By.name("keyword")).sendKeys("研修");
+
+		//エビデンスを取得する
+		getEvidence(new Object() {
+		}, "01");
+
+		//検索ボタンを押下する
+		WebDriverUtils.webDriver.findElement(By.cssSelector("input[type='submit']")).click();
+
+		//表示の待機時間
+		By questionText = By.cssSelector("dt.mb10 span:nth-of-type(2)");
+		visibilityTimeout(questionText, 5);
+
+		//検索結果が期待値と一致することを確認
+		List<WebElement> qestionTextList = WebDriverUtils.webDriver.findElements(questionText);
+		assertEquals(2, qestionTextList.size());
+		assertEquals("助成金書類の作成方法が分かりません", qestionTextList.get(0).getText());
+		assertEquals("研修の申し込みはどのようにすれば良いですか？", qestionTextList.get(1).getText());
+
+		//エビデンスを取得する
+		getEvidence(new Object() {
+		}, "02");
+
 	}
-	
+
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 「クリア」ボタン押下で入力したキーワードを消去")
 	void test06() {
-		// TODO ここに追加
+		//クリアボタン押下前の入力フォームの状態を確認
+		String formTextBefore = WebDriverUtils.webDriver.findElement(By.name("keyword")).getAttribute("value");
+		assertEquals("研修", formTextBefore);
+
+		//エビデンスを取得する
+		getEvidence(new Object() {
+		}, "01");
+
+		//クリアボタンを押下する
+		WebDriverUtils.webDriver.findElement(By.cssSelector("input[type='button']")).click();
+
+		//フォームが空になるまで待機
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.attributeToBe(By.name("keyword"), "value", ""));
+
+		//入力フォームの中身が消えていることを確認
+		String formTextAfter = WebDriverUtils.webDriver.findElement(By.name("keyword")).getAttribute("value");
+		assertEquals("", formTextAfter);
+
+		//エビデンスを取得する
+		getEvidence(new Object() {
+		}, "02");
 	}
 
 }
